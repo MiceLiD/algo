3ea7e38c3e9a21d6594e0a24f74e873f76692df8
lidongsevenlee <lidongsevenlee@didiglobal.com>
2023-08-14 10:56:58 +0800

    ✨ op bubble

--- src/algo/array-sort/bubble.ts
+++ src/algo/array-sort/bubble.ts
@@ -7,13 +7,16 @@
 function bubbleSort(arr: number[]): number[] {
   const len = arr.length
   for (let i = 0; i < len; i++) {
+    let swapped = false
     for (let j = 0; j < len - i - 1; j++) {
       if (arr[j + 1] < arr[j]) {
         const temp = arr[j]
         arr[j] = arr[j + 1]
         arr[j + 1] = temp
+        swapped = true
       }
     }
+    if (!swapped) break
   }
   return arr
 }
